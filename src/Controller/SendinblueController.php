72c58380aafb7ebddb89b0a1867a4f25fac37c3c
LE MEUT <loic.lemeut@outlook.com>
2017-04-06 21:32:12 +0200

    Fix access control if not loggedInState

--- src/Controller/SendinblueController.php
+++ src/Controller/SendinblueController.php
@@ -2,13 +2,25 @@
 
 namespace Drupal\sendinblue\Controller;
 
+use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\sendinblue\SendinblueManager;
 
 /**
  * Returns responses for entity browser routes.
  */
 class SendinblueController extends ControllerBase {
+
+  /**
+   * Checks access for a specific request.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   Run access checks for this account.
+   */
+  public function access(AccountInterface $account) {
+    return AccessResult::allowedIf(SendinblueManager::isLoggedInState());
+  }
 
   /**
    * Return cusotm page if user ligin or logout.
